c49f2d1cfd080165c944bb5c57564bc887dcae39
gmdnko003 <gmdnko003@sl-dual-176.cs.uct.ac.za>
2015-05-13 15:14:09 +0200

    Updating methods to comply with required output

---
+++ NetBeansProjects/GraphsAssignment/src/GraphsAssignment.java
@@ -0,0 +1,241 @@
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author gmdnko003
+ */
+
+// Graph class: evaluate shortest paths.
+//
+// CONSTRUCTION: with no parameters.
+//
+// ******************PUBLIC OPERATIONS**********************
+// void addEdge( String v, String w, double cvw )
+//                              --> Add additional edge
+// void printPath( String w )   --> Print path after alg is run
+
+// ******************ERRORS*********************************
+// Some error checking is performed to make sure graph is ok,
+// and to make sure graph satisfies properties needed by each
+// algorithm.  Exceptions are thrown if errors are detected.
+
+public class GraphsAssignment {
+    
+    public static final double INFINITY = Double.MAX_VALUE;
+    private Map<String,Vertex> vertexMap = new HashMap<String,Vertex>( );
+    /**
+     * Add a new edge to the graph.
+     */
+    public void addEdge( String sourceName, String destName, double cost )
+    {
+        Vertex v = getVertex( sourceName );
+        Vertex w = getVertex( destName );
+        v.adj.add( new Edge( w, cost ) );
+    }
+    
+    /**
+     * Driver routine to handle unreachables and print total cost.
+     * It calls recursive routine to print shortest path to
+     * destNode after a shortest path algorithm has run.
+     */
+    public void printPath( String destName )
+    {
+        Vertex w = vertexMap.get( destName );
+        if( w == null )
+            throw new NoSuchElementException( "Destination vertex not found" );
+        else if( w.dist == INFINITY )
+            System.out.println( destName + " is unreachable" );
+        else
+        {
+            System.out.print( "(Cost is: " + w.dist + ") " );
+            printPath( w );
+            System.out.println( );
+        }
+    }
+
+    /**
+     * If vertexName is not present, add it to vertexMap.
+     * In either case, return the Vertex.
+     */
+    private Vertex getVertex( String vertexName )
+    {
+        Vertex v = vertexMap.get( vertexName );
+        if( v == null )
+        {
+            v = new Vertex( vertexName );
+            vertexMap.put( vertexName, v );
+        }
+        return v;
+    }
+
+    /**
+     * Recursive routine to print shortest path to dest
+     * after running shortest path algorithm. The path
+     * is known to exist.
+     */
+    private void printPath( Vertex dest )
+    {
+        if( dest.prev != null )
+        {
+            printPath( dest.prev );
+            System.out.print( " to " );
+        }
+        System.out.print( dest.name );
+    }
+    
+    /**
+     * Initializes the vertex output info prior to running
+     * any shortest path algorithm.
+     */
+    private void clearAll( )
+    {
+        for( Vertex v : vertexMap.values( ) )
+            v.reset( );
+    }
+    
+    /**
+     * Process a request; return false if end of file.
+     */
+    public static boolean processRequest( Scanner in, GraphsAssignment g )
+    {
+        try
+        {
+            
+        }
+        catch( NoSuchElementException e )
+          { return false; }
+        return true;
+    }
+    
+    
+    /**
+     * A main routine that:
+     * 1. Reads a file containing edges (supplied as a command-line parameter);
+     * 2. Forms the graph;
+     * 3. Repeatedly prompts for two vertices and
+     *    runs the shortest path algorithm.
+     * The data file is a sequence of lines of the format
+     *    source destination cost
+     */
+    public static void main(String[] args) {
+        //(n-1)! solutions ie. 3! = 6 possible solutions
+        GraphsAssignment g;
+        g = new GraphsAssignment();
+        try
+        {   	
+            //FileReader fin = new FileReader(args[0]);
+        	FileReader fin = new FileReader("GraphsAssignmentExample.txt");
+            Scanner graphFile = new Scanner( fin );
+
+            // Read the edges and insert
+            String line;
+            while( graphFile.hasNextLine( ) )
+            {
+                line = graphFile.nextLine( );
+                StringTokenizer st = new StringTokenizer( line );
+
+                try
+                {
+                    if( st.countTokens( ) != 3 )
+                    {
+                        System.err.println( "Skipping ill-formatted line " + line );
+                        continue;
+                    }
+                    String source  = st.nextToken( );
+                    String dest    = st.nextToken( );
+                    int    cost    = Integer.parseInt( st.nextToken( ) );
+                    g.addEdge( source, dest, cost );
+                }
+                catch( NumberFormatException e )
+                  { System.err.println( "Skipping ill-formatted line " + line ); }
+             }
+         }
+         catch( IOException e )
+           { System.err.println( e ); }
+
+         System.out.println( "File read..." );
+         System.out.println( g.vertexMap.size( ) + " vertices" );
+
+         Scanner in = new Scanner( System.in );
+         while( processRequest( in, g ) )
+             ;
+    }
+    
+}
+
+// Used to signal violations of preconditions for
+// various shortest path algorithms.
+class GraphException extends RuntimeException
+{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public GraphException( String name )
+    {
+        super( name );
+    }
+}
+
+// Represents an edge in the graph.
+class Edge
+{
+    public Vertex     dest;   // Second vertex in Edge
+    public double     cost;   // Edge cost
+    
+    public Edge( Vertex d, double c )
+    {
+        dest = d;
+        cost = c;
+    }
+}
+
+// Represents an entry in the priority queue for Dijkstra's algorithm.
+class Path implements Comparable<Path>
+{
+    public Vertex     dest;   // w
+    public double     cost;   // d(w)
+    
+    public Path( Vertex d, double c )
+    {
+        dest = d;
+        cost = c;
+    }
+    
+    public int compareTo( Path rhs )
+    {
+        double otherCost = rhs.cost;
+        
+        return cost < otherCost ? -1 : cost > otherCost ? 1 : 0;
+    }
+}
+
+// Represents a vertex in the graph.
+class Vertex
+{
+    public String     name;   // Vertex name
+    public List<Edge> adj;    // Adjacent vertices
+
+    public double     dist;   // Cost
+    public Vertex     prev;   // Previous vertex on shortest path
+    public int        scratch;// Extra variable used in algorithm
+
+    public Vertex( String nm )
+      { name = nm; adj = new LinkedList<Edge>( ); reset( ); }
+
+    public void reset( )
+    //  { dist = Graph.INFINITY; prev = null; pos = null; scratch = 0; }    
+    { dist = GraphsAssignment.INFINITY; prev = null; scratch = 0; }
+      
+   // public PairingHeap.Position<Path> pos;  // Used for dijkstra2 (Chapter 23)
+}
