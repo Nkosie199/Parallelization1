d164bb2b447b2b172adcfd379d7613a69ed36bcc
gmdnko003 <gmdnko003@sl-dual-177.cs.uct.ac.za>
2015-08-10 20:17:17 +0200

    Finished testing....

--- NetBeansProjects/ParallelProgramming1/src/NaiveSolution.java
+++ NetBeansProjects/ParallelProgramming1/src/NaiveSolution.java
@@ -1,69 +1,69 @@
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Arrays;
 
 /**
- *
  * @author gmdnko003
  */
 public class NaiveSolution {
     
     static int noOfItems; //the number of items in the text file
-    static int filterNo = 3; //the variable filter value
-    static List<Integer> intArrayIndex; //the integer index
-    static List<Integer> intArrayValue; //the integer value
-    static boolean j = false; //boolean to find if arrayList is past the first index
+    static float[] intArrayValues2; //the new array of integer values with median filtering applied
+    static float nsTime;
     
-    public static void main (String[] args){
-        try {
-            load("inp1.txt");
-            //naiveMethod();
-        } catch (IOException ex) {
-            Logger.getLogger(NaiveSolution.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+    static long startTime = 0;
+	
+	private static void tick(){
+		startTime = System.currentTimeMillis();
+	}
+	private static float tock(){
+		return (System.currentTimeMillis() - startTime) / 1000.0f; 
+	}
     
-    public static void load(String filename) throws FileNotFoundException, IOException { 
-        List<String> lineList;
+    
+    public static void naiveMethod(float[] list, int filterNo) { //naive method takes parameters ArrayList and filter size, returns array of values
+        int median = (filterNo/2); //the median value
+        int low= 0; //index of first element in filter
+        int hi = filterNo; //index of last element in filter
+        int immutables = (filterNo/2); //the number of variables that will no change given their filters
+        tick();
         
-        String path = System.getProperty("user.dir")+"/"+filename; //to get working file directory in all use case
-        //Eg. "/home/g/gmdnko003/NetBeansProjects/HashingAssignment/src/lexicon.txt"
-        System.out.println(path);
-        
-        try (FileReader fileReader = new FileReader(path)) {
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String inputLine; //each line in the text file
-            lineList = new ArrayList<String>(); //each line including the word, word type and defintion is an array element
-            while ((inputLine = bufferedReader.readLine()) != null) {
-                System.out.println(inputLine);
-                lineList.add(inputLine); //now we have an ArrayList of data inputs
-                if (j==true){
-                    String[] stringArray = inputLine.split(" ");
-                    System.out.println(stringArray[0]);
-                    System.out.println(stringArray[1]);
-                }         
-                j= true;
-                //intArrayIndex.add(Integer.parseInt(stringArray[0])); //add text file index
-                //intArrayValue.add(Integer.parseInt(stringArray[1]));
-            }
-            noOfItems = Integer.parseInt(lineList.get(0));
-            System.out.println(lineList.get(0));
-        }
-        catch(Exception e){
-            System.out.println("File not found!");
-        }
-    }
-
-    private static void naiveMethod() {
         for (int i=0; i<noOfItems; i++){
-            
-        }
+            if (i<immutables){ //if array index is in the range of the first set of immutable items
+                intArrayValues2[i] = list[i];
+            }
+            else if(i<(noOfItems-(immutables)) & i>=immutables){ //condition to apply median filtering
+                float[] filter = new float[filterNo]; //new interger array of filterable elements 
+                int j = 0; //iterator variable for filter array
+                for (int k=low; k<hi; k++){
+                    filter[j] = list[k];
+                    j++;
+                }
+                Arrays.sort(filter);
+                //System.out.println(intArrayValues2[i]);
+                intArrayValues2[i] = (filter[median]); //-1 to get the array index
+                low++;
+                hi++;
+            }
+            else{
+                intArrayValues2[i] = list[i];
+            }
+        }// now I have a mean filtered array of values called intArrayValues2!!!
+        tock();
+        float time = tock();
+        nsTime = time;
+        System.out.println("Start of naive solution...");
+	System.out.println("Run took "+ time +" seconds");
+        System.out.println("End of naive solution...");
+        System.out.println("");
     }
     
+    /*
+        this method will debug print the original array from the text file with its relevant index
+    */
+    private static void debugPrint(int[] intArrayIndex, float[] arrayValue){
+        System.out.println("Debug printing...");
+        for (int i=0; i<arrayValue.length; i++){
+            System.out.println(intArrayIndex[i]+" : "+arrayValue[i]);
+        }
+    }
 }
